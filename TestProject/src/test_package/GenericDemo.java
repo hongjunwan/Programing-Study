7724635d3ce8ac11725deabed3f7735c8c7f78bf
dhks2 <dhks2@DESKTOP-FHGTLH4>
2020-05-04 14:58:44 +0900

    GenericDemo 수정

--- TestProject/src/test_package/GenericDemo.java
+++ TestProject/src/test_package/GenericDemo.java
@@ -2,13 +2,31 @@ package test_package;
 
 //클래스를 정의 할 때는 info의 데이터 타입을 확정하지 않고 인스턴스를 생성할 때 데이터 타입을 지정하는 기능이 제네릭이다.
 
+class StudentInfo{
+	public int grade;
+	StudentInfo(int grade){
+		this.grade = grade;
+	}
+}
+
+class EmployeeInfo{
+	public int rank;
+	EmployeeInfo(int rank){
+		this.rank = rank;
+	}
+}
+
 class Person<T>{
 	public T info;
+	Person(T info){
+		this.info = info;
+	}
 }
 
 public class GenericDemo {
 	public static void main(String[] args) {
-		Person<String> p1 = new Person<String>(); // 인스턴스 생성 시 데이터 타입 지정
-		Person<StringBuilder> p2 = new Person<StringBuilder>();
+		Person<EmployeeInfo> p1 = new Person<EmployeeInfo>(new EmployeeInfo(1));
+		EmployeeInfo e1 = p1.info;
+		System.out.println(e1.rank);
 	}
 }
